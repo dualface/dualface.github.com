9089679f606aee31ea94de8b601abe2f4f42e2a3
dualface <dualface@gmail.com>
2012-12-14 23:08:37 +0800

    move

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -3,25 +3,22 @@ var github = (function(){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+(repos[i].description||'')+'</p></li>';
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
       $.ajax({
-          url: "https://api.github.com/users/"+options.user+"/repos?type=pushed"
-        , type: 'json'
-        , error: function (err) {
-          $(options.target + ' li.loading').addClass('error').text("Error loading feed, " + err. responseText());
-        }
+          url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
+        , type: 'jsonp'
+        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.length) { return; }
-          for (var i = 0; i < data.length; i++) {
-            if (options.skip_forks && data[i].fork) { continue; }
-            if (data[i].description == "") { continue; }
-            repos.push(data[i]);
+          if (!data || !data.data) { return; }
+          for (var i = 0; i < data.data.length; i++) {
+            if (options.skip_forks && data.data[i].fork) { continue; }
+            repos.push(data.data[i]);
           }
           repos.sort(function(a, b) {
             var aDate = new Date(a.pushed_at).valueOf(),
